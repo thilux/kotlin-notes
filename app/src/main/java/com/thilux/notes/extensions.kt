d42c80ca260c93d4744ed94ba74cf2a0a64ed177
Thilux <thilux.systems@gmail.com>
2018-03-03 11:52:16 -0300

    Database open helper reference added to application context

--- app/src/main/java/com/thilux/notes/extensions.kt
+++ app/src/main/java/com/thilux/notes/extensions.kt
@@ -1,5 +1,14 @@
 package com.thilux.notes
 
+import android.content.Context
+import com.thilux.notes.db.NotesDatabaseOpenHelper
+
 /**
  * Created by tsantana on 03/03/18.
  */
+
+/**
+ * Extension for the application context to add a reference to the database open helper
+ */
+val Context.database: NotesDatabaseOpenHelper
+    get() = NotesDatabaseOpenHelper.getInstance(applicationContext)
